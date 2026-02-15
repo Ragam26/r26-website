9d90404e302021e3ecc5d953c398359d4423ecc0
arunnats <arunnats2004@gmail.com>
2026-02-16 03:02:53 +0530

    [chore] integration bugs

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,18 @@
-import PolaroidPage from '../pageComponents/polaroid/page'
-import LandingPage from '@/pageComponents/landing/LandingPage'
-import Legacy from '@/pageComponents/legacy/Legacy'
-import ProshowSection from '@/pageComponents/proshow/proshow'
+import PolaroidPage from "../pageComponents/polaroid/page";
+import LandingPage from "@/pageComponents/landing/LandingPage";
+import Legacy from "@/pageComponents/legacy/Legacy";
+import ProshowSection from "@/pageComponents/proshow/proshow";
+import ProgramCarousel from "@/pageComponents/carousel/ProgramCarousel";
 
 export default function Home() {
   return (
     <>
-      {' '}
+      {" "}
       <LandingPage />
       <PolaroidPage />
       <ProgramCarousel />
       <ProshowSection />
       <Legacy />
     </>
-  )
+  );
 }
