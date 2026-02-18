096d7f0101959a815ee44313f4d7610100238bc6
mathew <amonline2005@gmail.com>
2026-02-18 14:49:02 +0530

    feat:Made CA-page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import LandingPage from '@/pageComponents/landing/LandingPage'
 import Legacy from '@/pageComponents/legacy/Legacy'
 import ProshowSection from '@/pageComponents/proshow/proshow'
 import ProgramCarousel from '@/pageComponents/carousel/ProgramCarousel'
-import CampusAmbassador from '@/pageComponents/campusAmbassador/CampusAmbassador'
 
 export default function Home() {
   return (
@@ -14,7 +13,6 @@ export default function Home() {
       <ProgramCarousel />
       <ProshowSection />
       <Legacy />
-      <CampusAmbassador />
     </>
   )
 }
