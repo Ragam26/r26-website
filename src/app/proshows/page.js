c25e075a81c962875c40f0f7d9f1cf24e30e6edd
Aravind <aravindanil2101@gmail.com>
2026-03-12 22:08:59 +0530

    day 3 updates

--- src/app/proshows/page.js
+++ src/app/proshows/page.js
@@ -30,8 +30,20 @@ const PROSHOW_EVENTS = [
     earlyBirdFee: '1199',
     artists: ['DARSHAN RAVAL', 'VEDAN'],
     images: ['/images/proshow/day2/Darshan.jpg', '/images/proshow/day2/Vedan.jpg'],
+  },
+  {
+    id: 3,
+    day: '3',
+    divasam: 'SUNDAY',
+    date: '29',
+    alignment: 'left',
+    regUrl: registrationUrl,
+    regFee: '1499',
+    earlyBirdFee: '1199',
+    artists: ['JONITA GANDHI', 'VINEETH SREENIVASAN', 'YOGISEKAR'],
+    images: ['/images/proshow/day3/Jonita.webp', '/images/proshow/day3/Vineeth.webp','/images/proshow/day3/yogi.webp'],
   }
-  // Add more days here...
+  
 ]
 
 export default function EventsPage() {
