c20f668fd470db31b4bec3dc2f2d5cff949e92d8
mathew <amonline2005@gmail.com>
2026-03-13 16:02:28 +0530

    fix:route added for alumniConclaves

--- src/pageComponents/carousel/ProgramCarousel.jsx
+++ src/pageComponents/carousel/ProgramCarousel.jsx
@@ -13,19 +13,19 @@ gsap.registerPlugin(CustomEase)
 const ProgramCarousel = () => {
   const router = useRouter()
   const slideRoutes = [
-    "/events",
-    "/workshops",
-    "/sahiti",
-    "/notFound",
-    "/notFound",
-    "/notFound",
-    "/notFound",
-    "/i-ink",
-    "/notFound",
-  ];
-  const totalSlides = 8;
-  const [currentSlide, setCurrentSlide] = useState(1);
-  const isAnimating = useRef(false);
+    '/events',
+    '/workshops',
+    '/sahiti',
+    '/notFound',
+    '/alumniconclaves',
+    '/notFound',
+    '/notFound',
+    '/i-ink',
+    '/notFound',
+  ]
+  const totalSlides = 8
+  const [currentSlide, setCurrentSlide] = useState(1)
+  const isAnimating = useRef(false)
 
   const createSlide = (slideNumber, direction) => {
     const slide = document.createElement('div')
