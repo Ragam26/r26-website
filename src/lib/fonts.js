446ac24203a0c3688828c64f3c6b903910b2d5dd
Aravind <aravindanil2101@gmail.com>
2026-03-23 19:45:59 +0530

    font optimization

--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -11,73 +11,73 @@ import {
 } from "next/font/google";
 
 export const neuemachina = localFont({
-  src: "../../public/fonts/NeueMachina.otf",
+  src: "../../public/fonts/NeueMachina.woff2",
   variable: "--font-neuemachina",
   display: "swap",
 });
 
 export const impact = localFont({
-  src: "../../public/fonts/impact.ttf",
+  src: "../../public/fonts/impact.woff2",
   variable: "--font-impact",
   display: "swap",
 });
 
 export const archivo = localFont({
-  src: "../../public/fonts/ArchivoBlack.ttf",
+  src: "../../public/fonts/ArchivoBlack.woff2",
   variable: "--font-archivo",
   display: "swap",
 });
 
 export const instrument = localFont({
-  src: "../../public/fonts/InstrumentSerif.ttf",
+  src: "../../public/fonts/InstrumentSerif.woff2",
   variable: "--font-instrument",
   display: "swap",
 });
 
 export const brixton = localFont({
-  src: "../../public/fonts/brixton.otf",
+  src: "../../public/fonts/brixton.woff2",
   variable: "--font-brixton",
   display: "swap",
 });
 
 export const calfine = localFont({
-  src: "../../public/fonts/calfine.ttf",
+  src: "../../public/fonts/calfine.woff2",
   variable: "--font-calfine",
   display: "swap",
 });
 
 export const elanor = localFont({
-  src: "../../public/fonts/elanorFreePersonalUse-ExtLt.otf",
+  src: "../../public/fonts/elanorFreePersonalUse-ExtLt.woff2",
   variable: "--font-elanor",
   display: "swap",
 });
 
 export const magilio = localFont({
-  src: "../../public/fonts/magilioRegular-8Mxvg.otf",
+  src: "../../public/fonts/magilioRegular-8Mxvg.woff2",
   variable: "--font-magilio",
   display: "swap",
 });
 
 export const marko = localFont({
-  src: "../../public/fonts/markoOne-Regular.ttf",
+  src: "../../public/fonts/markoOne-Regular.woff2",
   variable: "--font-marko",
   display: "swap",
 });
 
 export const moniqa = localFont({
-  src: "../../public/fonts/moniqa-SemBdCondDisplay.otf",
+  src: "../../public/fonts/moniqa-SemBdCondDisplay.woff2",
   variable: "--font-moniqa",
   display: "swap",
 });
 
 export const scalter = localFont({
-  src: "../../public/fonts/scalter-SerifCondensedSlanted.otf",
+  src: "../../public/fonts/scalter-SerifCondensedSlanted.woff2",
   variable: "--font-scalter",
   display: "swap",
 });
 
 export const kiwi = localFont({
-  src: "../../public/fonts/KiwiMaru-Medium.ttf",
+  src: "../../public/fonts/KiwiMaru-Medium.woff2",
   variable: "--font-kiwi",
   display: "swap",
 });
@@ -89,12 +89,12 @@ export const slackey = Slackey({
 });
 
 export const leagueGothic = localFont({
-  src: "../../public/fonts/leagueGothic-Regular-VariableFont_wdth.ttf",
+  src: "../../public/fonts/leagueGothic-Regular-VariableFont_wdth.woff2",
   variable: "--font-league-gothic",
 });
 
 export const reykjavik = localFont({
-  src: "../../public/fonts/SK-Reykjavik-Rounded-Regular.ttf",
+  src: "../../public/fonts/SK-Reykjavik-Rounded-Regular.woff2",
   variable: "--font-reykjavik",
 });
 
@@ -122,13 +122,13 @@ export const poppins = Poppins({
 });
 
 export const liberationSerif = localFont({
-  src: "../../public/fonts/LiberationSerif-Bold.ttf",
+  src: "../../public/fonts/LiberationSerif-Bold.woff2",
   variable: "--font-liberation-serif",
   display: "swap",
 });
 
 export const grotapDemo = localFont({
-  src: "../../public/fonts/GrotapDemo.otf",
+  src: "../../public/fonts/GrotapDemo.woff2",
   variable: "--font-grotap-demo",
   display: "swap",
 });
